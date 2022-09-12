633311fb3e61b6c1173b3fe7889e7e4e5c38b64d
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2022-09-12 15:13:05 -0500

    primera correccion

--- front-end/src/admin/components/pagos/MethodPay.jsx
+++ front-end/src/admin/components/pagos/MethodPay.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const MethodPay = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+        <div className='d-flex justify-content-sm-center align-items-lg-center gap-3 mt-4'>
+            <input type="text" className='form-control w-50 '/>
+            <button className='btn btn-success'>Buscar por Curso</button>
+            <button className='btn btn-success'>Buscar por Usuario</button>
+        </div>
+        <div className='mt-4'>
+            <table className='table table-striped'>
+                <thead className='table-dark'>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </>
   )
 }
 
--- front-end/src/admin/pages/MethodPayPage.jsx
+++ front-end/src/admin/pages/MethodPayPage.jsx
@@ -6,7 +6,6 @@ import './style.css'
 const MethodPayPage = () => {
   return (
     <div className={`p-4 my-container`}>
-      <h1 className="text-center">Metodo de Pago </h1>
       <div className="img-pago">
         <img src={imagePago} alt="pago" />
       </div>
