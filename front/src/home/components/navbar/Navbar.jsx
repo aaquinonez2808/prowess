44b14f7434aafcf996b0d04e100997aba40c1e2d
Aliset1 <alvargas3@espe.edu.ec>
2022-10-06 10:22:58 -0500

    logo

--- front/src/home/components/navbar/Navbar.jsx
+++ front/src/home/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/img/logo1.webp";
+import logo from "../../../assets/images/logo.png"
 import "./css.css";
 export const Navbar = () => {
   return (
