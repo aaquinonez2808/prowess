56e42dda415fb77525b45251707c4d72664af9b7
MLxFernando <fernandomacas68@gmail.com>
2022-10-11 11:07:23 -0500

    Actualizacion-registro

--- front/src/home/pages/RegistroPage.jsx
+++ front/src/home/pages/RegistroPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import inscripcion from '../assets/img/inscrip.jpg'
-import {NavbarRedes} from  '../components/navbar/NavbarRedes'
+import { NavbarRedes } from '../components/navbar/NavbarRedes'
 
 function RegistroPage() {
   const [page, setPage] = React.useState(1);
@@ -12,6 +12,11 @@ function RegistroPage() {
   const handlePrev = () => {
     setPage((prevPage) => prevPage - 1);
   };
+
+  const Registrar =(e) =>{
+    e.preventDefault();
+    console.log(e);
+  };  
 
   return (
 
@@ -26,51 +31,51 @@ function RegistroPage() {
         <img width="100%" height="100%" src={inscripcion} alt="inscripcion" />
       </div>
       <div className="col-md-6 col-xs-12">
-        <form className="form" id="form" >
+        <form className="form" id="form" onSubmit={Registrar}>
           <p className="form-titulo">{page}/2 Regístrate</p>
           {
             page === 1 && (
               <>
-                <div className="form-input myname">
-                  <label htmlFor="myname">Nombre</label>
-                  <input type="text" name="Nombres" placeholder="Nombre" ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-input surname">
-                  <label htmlFor="surname">Apellidos</label>
-                  <input type="text" name="Apellidos" placeholder="Apellidos" ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-input email">
-                  <label htmlFor="email">Correo electrónico</label>
-                  <input type="email" name="Email" placeholder="Email" ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-input mobile">
-                  <label htmlFor="mobile">Celular</label>
-                  <input type="text" name="Celular" placeholder="Celular" ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-input password">
-                  <label htmlFor="password">Contraseña</label>
-                  <input type="password" name="Contraseñ" placeholder="Contraseña"  ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-input repeatPassword">
-                  <label htmlFor="repeatPassword">Repetir Contraseña</label>
-                  <input type="password" name="Contraseña" placeholder="Contraseña" ></input>
-                  <p className="mensajeError"></p>
-                </div>
-                <div className="form-regist-select mb-4" >
-                  <select className="form-select" >
-                    <option  >Pais</option>
-                    <option value="">2</option>
-                    <option value=""> 3</option>
-                  </select>
-                </div>
-                <div className="form-boton d-flex justify-content-end ">
-                  <button type="button" className="" target="_blank" onClick={handleNext}>Siguiente</button>
-                </div>
+                  <div className="form-input myname">
+                    <label htmlFor="myname">Nombres</label>
+                    <input type="text" name="Nombres" placeholder="Nombres"></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-input surname">
+                    <label htmlFor="surname">Apellidos</label>
+                    <input type="text" name="Apellidos" placeholder="Apellidos" ></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-input email">
+                    <label htmlFor="email">Correo electrónico</label>
+                    <input type="email" name="Email" placeholder="Email" ></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-input mobile">
+                    <label htmlFor="mobile">Cedula</label>
+                    <input type="number" name="Cedula" placeholder="Cedula" ></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-input password">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" name="Contraseñ" placeholder="Contraseña"  ></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-input repeatPassword">
+                    <label htmlFor="repeatPassword">Repetir Contraseña</label>
+                    <input type="password" name="Contraseña" placeholder="Contraseña" ></input>
+                    <p className="mensajeError"></p>
+                  </div>
+                  <div className="form-regist-select mb-4" >
+                    <select className="form-select" >
+                      <option  >Pais</option>
+                      <option value="">2</option>
+                      <option value=""> 3</option>
+                    </select>
+                  </div>
+                  <div className="form-boton d-flex justify-content-end ">
+                    <button type="button" className="" target="_blank" onClick={handleNext}>Siguiente</button>
+                  </div>
               </>
             )
           }
@@ -140,9 +145,9 @@ function RegistroPage() {
                   </div>
                 </div>
                 <div className=" row mb-4 gap-2 ">
-                  <div className="">     
-                  <input type="checkbox" name="terminos" id="terminos" value="Terminos" />    
-                    <a href="/terminos" target="_blank" rel="noreferrer">He Leído y Acepto los Términos y Condiciones </a>                    
+                  <div className="">
+                    <input type="checkbox" name="terminos" id="terminos" value="Terminos" />
+                    <a href="/terminos" target="_blank" rel="noreferrer">He Leído y Acepto los Términos y Condiciones </a>
                   </div>
                 </div>
                 <div className='d-flex justify-content-between'>
@@ -150,7 +155,7 @@ function RegistroPage() {
                     <button type="button" className="" target="_blank" onClick={handlePrev}>Atras</button>
                   </div>
                   <div className="form-boton ">
-                    <button type="button" className="" target="_blank" onClick={handlePrev}>Registrar</button>
+                    <button type="Submit" className="">Registrar</button>
                   </div>
                 </div>
 
