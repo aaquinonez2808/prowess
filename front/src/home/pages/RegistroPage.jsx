72ccbc9a312f11433b9e4ae38369baf7bc89ff6b
aby456 <adquillupangui@espe.edu.ec>
2022-10-11 12:03:07 -0500

    userform

--- front/src/home/pages/RegistroPage.jsx
+++ front/src/home/pages/RegistroPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import inscripcion from '../assets/img/inscrip.jpg'
 import { NavbarRedes } from '../components/navbar/NavbarRedes'
+import { useForm } from "react-hook-form";
 
 function RegistroPage() {
   const [page, setPage] = React.useState(1);
@@ -13,10 +14,11 @@ function RegistroPage() {
     setPage((prevPage) => prevPage - 1);
   };
 
-  const Registrar =(e) =>{
-    e.preventDefault();
+  const Registrar = (e) => {
     console.log(e);
-  };  
+  };
+
+  const { register,  handleSubmit, formState: { errors } } = useForm();
 
   return (
 
@@ -31,51 +33,50 @@ function RegistroPage() {
         <img width="100%" height="100%" src={inscripcion} alt="inscripcion" />
       </div>
       <div className="col-md-6 col-xs-12">
-        <form className="form" id="form" onSubmit={Registrar}>
+        <form className="form" id="form" onSubmit={handleSubmit(Registrar)}>
           <p className="form-titulo">{page}/2 Regístrate</p>
           {
             page === 1 && (
               <>
-                  <div className="form-input myname">
-                    <label htmlFor="myname">Nombres</label>
-                    <input type="text" name="Nombres" placeholder="Nombres"></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-input surname">
-                    <label htmlFor="surname">Apellidos</label>
-                    <input type="text" name="Apellidos" placeholder="Apellidos" ></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-input email">
-                    <label htmlFor="email">Correo electrónico</label>
-                    <input type="email" name="Email" placeholder="Email" ></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-input mobile">
-                    <label htmlFor="mobile">Cedula</label>
-                    <input type="number" name="Cedula" placeholder="Cedula" ></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-input password">
-                    <label htmlFor="password">Contraseña</label>
-                    <input type="password" name="Contraseñ" placeholder="Contraseña"  ></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-input repeatPassword">
-                    <label htmlFor="repeatPassword">Repetir Contraseña</label>
-                    <input type="password" name="Contraseña" placeholder="Contraseña" ></input>
-                    <p className="mensajeError"></p>
-                  </div>
-                  <div className="form-regist-select mb-4" >
-                    <select className="form-select" >
-                      <option  >Pais</option>
-                      <option value="">2</option>
-                      <option value=""> 3</option>
-                    </select>
-                  </div>
-                  <div className="form-boton d-flex justify-content-end ">
-                    <button type="button" className="" target="_blank" onClick={handleNext}>Siguiente</button>
-                  </div>
+                <div className="form-input myname">
+                  <label htmlFor="myname">Nombres</label>
+                  <input type="text" placeholder="Nombres" {...register("nombre", { required: true })}></input>
+                </div>
+                <div className="form-input surname">
+                  <label htmlFor="surname">Apellidos</label>
+                  <input type="text" name="Apellidos" placeholder="Apellidos" ></input>
+                  <p className="mensajeError"></p>
+                </div>
+                <div className="form-input email">
+                  <label htmlFor="email">Correo electrónico</label>
+                  <input type="email" name="Email" placeholder="Email" ></input>
+                  <p className="mensajeError"></p>
+                </div>
+                <div className="form-input mobile">
+                  <label htmlFor="mobile">Cedula</label>
+                  <input type="number" name="Cedula" placeholder="Cedula" ></input>
+                  <p className="mensajeError"></p>
+                </div>
+                <div className="form-input password">
+                  <label htmlFor="password">Contraseña</label>
+                  <input type="password" name="Contraseñ" placeholder="Contraseña"  ></input>
+                  <p className="mensajeError"></p>
+                </div>
+                <div className="form-input repeatPassword">
+                  <label htmlFor="repeatPassword">Repetir Contraseña</label>
+                  <input type="password" name="Contraseña" placeholder="Contraseña" ></input>
+                  <p className="mensajeError"></p>
+                </div>
+                <div className="form-regist-select mb-4" >
+                  <select className="form-select" >
+                    <option  >Pais</option>
+                    <option value="">2</option>
+                    <option value=""> 3</option>
+                  </select>
+                </div>
+                <div className="form-boton d-flex justify-content-end ">
+                  <button type="button" className="" target="_blank" onClick={handleNext}>Siguiente</button>
+                </div>
               </>
             )
           }
@@ -108,8 +109,8 @@ function RegistroPage() {
                 </div>
                 <div className="form-input mobile">
                   <label htmlFor="mobile">Direccion</label>
-                  <input type="text" name="Direccion" placeholder="Direccion" ></input>
-                  <p className="mensajeError"></p>
+                  <input type="text"  {...register("direccion", { required: true })} ></input>
+                  {errors.direccion && <span>This field is required</span>}
                 </div>
                 <div className="form-input mobile">
                   <label htmlFor="mobile">Celular</label>
