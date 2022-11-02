8b363fb03c18c79dc4c68b09041f59e0f5f2ab42
MLxFernando <fernandomacas68@gmail.com>
2022-11-02 16:57:50 -0500

    Institucion Update

--- front/src/home/components/nosotros/Nosotros.jsx
+++ front/src/home/components/nosotros/Nosotros.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import {Jumbotron} from "./jumbotron/Jumbotron";
-import {Detalles} from "./detalles/Detalles";
+import { Jumbotron } from "./jumbotron/Jumbotron";
+import { Detalles } from "./detalles/Detalles";
 import Footer from "../footer/Footer"
 
 export const Nosotros = () => {
@@ -9,21 +9,19 @@ export const Nosotros = () => {
     <>
       <main role="main" className="flex-shrink-0 mt-5">
 
-      <div className="container-xl">
-      <div className="row align-items-start">
-    <div className="col">
-    <Jumbotron />
-    </div>
-    <div className="col">
-    <Detalles />
-    </div>
-    
-  </div>
-</div>
-
+        <div className="container-xl">
+          <div className="row align-items-start">
+            <div className="col">
+              <Jumbotron />
+            </div>
+            <div className="col">
+              <Detalles />
+            </div>
+          </div>
+        </div>
       </main>
 
-  <Footer/>    
+      <Footer />
     </>
   );
 }
