7945163f314274f2ef689d22e54d562c95dfa687
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2022-11-14 17:39:19 -0500

    correccion

--- front/src/home/components/curso/Curso.jsx
+++ front/src/home/components/curso/Curso.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 const Curso = ({ curso }) => {
   return (
     <div className="col-lg-4 col-sm-6 col-xs-12">
-      <NavLink to={`/cursos/${curso.id}`} className="nav-link">
+      <NavLink to={`/cursos/${curso.tipo}/${curso.id}`} className="nav-link">
         <div className="card-body">
           <h5 className="card-title mb-0">{curso.nombre}</h5>
         </div>
