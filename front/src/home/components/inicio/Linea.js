eecab2f7e0448cf76577a25733321a3d8070fb63
ElverJavierMorenoSanchez <ejmoreno6@espe.edu.ec>
2022-12-20 16:32:21 -0500

    Actualizacion

---
+++ front/src/home/components/inicio/Linea.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Linea = () => {
+  return (
+    <div className="text-center mb-5 descripcion-curso">
+      <img
+        className=" "
+        src="https://res.cloudinary.com/dena7lqj3/image/upload/v1670622586/modulomocc/home/linea.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Linea;
