eb088d9029642ab030b0aef10df17714e446fc23
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2023-01-06 18:08:57 -0500

    Update Contenido.jsx

--- front/src/home/components/detalle curso/Contenido.jsx
+++ front/src/home/components/detalle curso/Contenido.jsx
@@ -5,31 +5,27 @@ import { NavLink } from "react-router-dom";
 const Contenido = ({ curso }) => {
   return (
     <>
-    <div className="row " id="principal">
-        <div className="col-md-6 col-xs-12 p-4 cabecera" >
-            <h1 className="title-nombre"> CURSO DE {curso.nombre}</h1>
-            <h4 align="left" className="text-modalidad "> Modalidad del Curso: {curso.modalidad}</h4>
-            <ul className="list-group">
-              {
-                curso.aprender && curso.aprender.map((aprend, index) => {
-                  return <li className="list-group-item text-start bg-none" key={index}>{aprend}</li>
-                })
-              }
-            </ul>
-            <div className=" btn-register">
-            <NavLink to={`/register`} className="nav-link" target="_blank" rel="noreferrer">  
+      <div className="row container-cursos" id="principal">
+        <div className="col-md-6 col-xs-12 p-4 cabecera">
+          <h1 className="title-nombre"> CURSO DE {curso.nombre}</h1>
+          <h4 align="left" className="text-modalidad"> Modalidad del Curso: {curso.modalidad}</h4>
+          <ul className="list-group">
+            {
+              curso.aprender && curso.aprender.map((aprend, index) => {
+                return <li className="list-group-item text-start bg-none" key={index}>{aprend}</li>
+              })
+            }
+          </ul>
+          <div className="btn-register">
+            <NavLink to={`/register`} className="nav-link" target="_blank" rel="noreferrer">
               <button className="button">INSCRÍBETE YA</button>
             </NavLink>
-            </div>  
+          </div>
         </div>
-        <div className="col-md-6 col-xs-12" id="prueba">  
-          <img 
-            src={curso.imagenCurso}
-            className=" p-0 w-100 h-100 "
-            alt="..."
-          />
+        <div className="col-md-6 col-xs-12" id="prueba">
+          <img src={curso.imagenCurso} className="p-0 w-100 h-100" alt="..." />
         </div>
-    </div>
+      </div>
     </>
   );
 };
