f86e2a2152e23d8e4793973d6aac6e5ec221ad7a
Lizeth Abigail Iza Moreno <84360524+Lizeth-Abigail-Iza-Moreno@users.noreply.github.com>
2023-01-24 16:50:18 -0500

    Update Slider.jsx

--- front/src/home/components/slider/Slider.jsx
+++ front/src/home/components/slider/Slider.jsx
@@ -1,33 +1,27 @@
 import React from "react";
-import Slider1 from "../../assets/img/sslider1.jpg";
-import Slider2 from "../../assets/img/slider2.jpg";
-import Slider3 from "../../assets/img/slider3.jpg";
 import "./style.css";
 export const Slider = () => {
   return (
     <div
       id="carouselExampleControls"
       className="carousel slide"
-      data-bs-ride="carousel"
-      
-    >
+      data-bs-ride="carousel">
       <div className="carousel-inner slider-img">
         <div className="carousel-item active" data-bs-interval="2000">
-          <img src={Slider1} className="d-block w-100" alt="..." />
+          <img src="https://res.cloudinary.com/dena7lqj3/image/upload/v1670622629/modulomocc/home/sslider1_g0yrxk.jpg" className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item" data-bs-interval="2000">
-          <img src={Slider2} className="d-block w-100" alt="..." />
+          <img src="https://res.cloudinary.com/dena7lqj3/image/upload/v1670622604/modulomocc/home/slider2_zmjagj.jpg" className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item" data-bs-interval="2000">
-          <img src={Slider3} className="d-block w-100" alt="..." />
+          <img src="https://res.cloudinary.com/dena7lqj3/image/upload/v1670622613/modulomocc/home/slider3_r8azlx.jpg" className="d-block w-100" alt="..." />
         </div>
       </div>
       <button
         className="carousel-control-prev"
         type="button"
         data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
+        data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
