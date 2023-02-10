312be06fe4bef03206605064ea7969ce02ad9cd3
Javier Moreno <ejmoreno6@espe.edu.ec>
2023-02-10 12:15:05 -0500

    heroku test

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import coursesRoutes from "./routes/courses.routes.js";
 import categoriesRoutes from "./routes/categories.routes.js";
-
 const app = express();
 
 app.use(express.json());
